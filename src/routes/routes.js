c971ed6d7cbe3ed3a2485dfe378fb68132675101
jorgeduje <jorgeduje4@gmail.com>
2023-05-10 09:53:12 -0300

    se agrego ruta login

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,15 @@
 import Home from "../components/page/home/Home";
+import Login from "../components/page/login/Login";
 
 export let routes = [
-    {id: "home", path: "/", Element: Home}
-]
-
+  {
+    id: "home",
+    path: "/",
+    Element: Home,
+  },
+  {
+    id: "login",
+    path: "/login",
+    Element: Login
+  }
+];
